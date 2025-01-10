45d76bc5371b13b8c01f0aa2add0d86aecbbce8d
Hskcodes <hskdashing@aol.com>
2025-01-10 23:50:51 +0500

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>╔════════════⦿\n├⋗ ᴄʀᴇᴀᴛᴏʀ : <a href='tg://user?id={7030439873}'>Mr Bankai </a>\n├⋗ ʟᴀɴɢᴜᴀɢᴇ : <code>Python3</code>\n├⋗ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio 2.0.106</a>\n├⋗ ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href=https://t.me/Mr_Bankaiiii>File Store Bot</a>\n├⋗ Main Channel : <a href=https://t.me/Anime_Fair>Anime Fair</a>\n├⋗ Support Group : <a href=https://t.me/Chat_Weeb>Chat Weebs</a>\n╚═════════════════⦿</b>",
+            text = f"<b>╔════════════⦿\n├⋗ ᴄʀᴇᴀᴛᴏʀ : <a href='tg://user?id={7030439873}'>zb network </a>\n├⋗ ʟᴀɴɢᴜᴀɢᴇ : <code>Python3</code>\n├⋗ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio 2.0.106</a>\n├⋗ ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href=https://t.me/zblivebot>File Store Bot</a>\n├⋗ Main Channel : <a href=https://t.me/zbbackup>Zb Network</a>\n├⋗ Support Group : <a href=https://t.me/+NdifFX8P1pMwOGY0>Chat Weebs</a>\n╚═════════════════⦿</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
